f7c6e334e6bd65b26d34942a8a0114c392ab7a77
Mika <makomitsuki@gmail.com>
2018-04-03 13:39:50 -0500

    mika's project 3

---
+++ MCSAAnswer.java
@@ -0,0 +1,28 @@
+package cs342hw3;
+import java.util.*;
+
+public class MCSAAnswer extends MCAnswer{
+	
+	public MCSAAnswer(String Text, double CreditIfSelected) {
+		super(Text, CreditIfSelected);
+	}
+	
+	public MCSAAnswer(Scanner scan)
+	{
+		super(scan);
+	}
+	
+	public double getCredit()
+	{
+		return super.getCredit();
+	}
+	/* !! REMOVED FROM MCSAAnswer
+	public double getCredit(Answer rightAnswer)
+	{
+		if (selected)
+			return creditIfSelected;
+		else
+			return 0.0;
+	} */
+
+}
