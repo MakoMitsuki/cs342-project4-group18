b569c28bc31dd8c7fbd1c22cd43261912b7eccda
mpedra230 <35666664+mpedra230@users.noreply.github.com>
2018-04-04 12:35:14 -0500

    Michael's NumAnswer

---
+++ NumAnswer.java
@@ -0,0 +1,63 @@
+// Michael Pedraza
+// mpedra5
+
+import java.util.*;
+import java.io.*;
+
+class NumAnswer extends Answer{
+  
+  protected double number;
+  protected double tolerance;
+  
+  public NumAnswer(double val, double tol)
+  {
+    number = val;
+    tolerance = tol;
+  
+  }
+  
+  NumAnswer (Scanner s1){
+    //System.out.println("enter an answer");
+    number = s1.nextDouble();
+    System.out.println(number);
+    // clear the line
+    s1.nextLine();
+    // get tolerance level
+    tolerance = s1.nextDouble();
+    
+  }
+  
+  public void print(){
+    System.out.println(number);
+  }
+  
+  //
+  // getCredit
+  //
+  
+  public double getCredit(Answer rightAnswer){
+    
+    
+    // cast first
+    NumAnswer theRightAnswer = (NumAnswer)rightAnswer;    
+    double upperBound=0, lowerBound =0;
+    double num = theRightAnswer.number;
+    double tol = theRightAnswer.tolerance;
+    
+    upperBound = num + tol;
+    lowerBound = num - tol;
+    
+    if(number <= upperBound && number >= lowerBound  ){
+      // correct
+      return 1.0;
+    }
+    return 0.0;
+  }
+  
+  public String getCorrect(){ 
+   return "";
+  }
+  
+  
+  
+}
